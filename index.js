4419875da83876480804a1265472f7e121dcaefb
Eric Rodriguez <emorque10@gmail.com>
2024-08-16 19:15:54 -0400

    Completed victory UI

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const hoverAudio = new Audio('sfx/menuHover.wav');
 const menuSelect = new Audio('sfx/menuSelect.wav');
 
 options.forEach(option => {
-    option.addEventListener("mouseover", (event) => {
+    option.addEventListener("mouseenter", (event) => {
         hoverAudio.play();
     })
 });
--- load.js
+++ load.js
@@ -4,7 +4,7 @@ const hoverAudio = new Audio('sfx/menuHover.wav');
 const menuSelect = new Audio('sfx/menuSelect.wav');
 
 options.forEach(option => {
-    option.addEventListener("mouseover", (event) => {
+    option.addEventListener("mouseenter", (event) => {
         hoverAudio.play();
     })
 });
