952d8b1ba90d67f5451ab1ed2e3418e4cce9a72d
Eric Rodriguez <emorque10@gmail.com>
2024-08-22 15:48:02 -0400

    Improved performance, accessibility, and seo

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const menuImage = document.getElementById('titleImage');
 let num = 9;
 
-menuImage.src = "img/bg6.jpg";
+menuImage.src = "img/bg6.webp";
 
 imagesArray = ["img/bg1.webp", "img/bg2.webp", "img/bg3.webp", "img/bg4.webp", "img/bg5.webp", "img/bg6.webp", "img/bg7.webp", "img/bg8.webp", "img/bg9.webp", "img/bg10.webp"];
 
